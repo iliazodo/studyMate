498a0cec1ca9cf09c22b88e5877436b58f55337e
iliazodo <zodozdm@gmail.com>
2025-06-15 19:49:55 -0700

    add user model.

---
+++ server/models/user.model.js
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  picture: {
+    type: String,
+  },
+  bio: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  lectures: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Lecture",
+  }],
+  communities: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Community",
+  }],
+  posts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+  }],
+  followers: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  }],
+  following: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  }],
+  questions: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Question",
+  }],
+  conversations:[{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Conversation"
+  }],
+  isVerified:{
+    type: Boolean,
+    default: false
+  },
+  isBan:{
+    type:Boolean,
+    default: false
+  },
+  blockedUsers:[{
+    type: mongoose.Schema.Types.ObjectId,
+    ref:"User",
+  }],
+  role:{
+    type: String,
+    enum: ["admin" , "user"],
+    default: "user"
+  }
+});
+
+const User = mongoose.model("User" , userSchema);
+
+export default User;
