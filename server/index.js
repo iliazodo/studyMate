2c660ffee5f5041adcb94bdd63d8d87a1da1569e
iliazodo <zodozdm@gmail.com>
2025-06-21 20:43:04 -0700

    add user router.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
-import connectToMongodb from './config/mongodb.js';
-import questionRoute from './routes/questionRoutes.js';
-import authRouter from './routes/auth.route.js';
+import connectToMongodb from "./config/mongodb.js";
+import questionRoute from "./routes/questionRoutes.js";
+import authRouter from "./routes/auth.route.js";
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -20,7 +21,8 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRouter);
-app.use("/api/questions", questionRoute); 
+app.use("/api/questions", questionRoute);
+app.use("/api/user" , userRouter);
 
 app.listen(port, () => {
   console.log("Server is running on port: " + port);
